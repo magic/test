de8d0f99736d600f073a399f7ccaae831d8e72dd
Jascha Ehrenreich <jascha@jaeh.at>
2018-04-15 20:27:51 +0200

    add promise tests

---
+++ test/lib/promise.js
@@ -0,0 +1,18 @@
+const { promise } = require("../../src")
+
+const fnWithCb = (err, arg, cb) => {
+  if (typeof err === "string") {
+    err = new Error(err)
+  }
+  cb(err, arg)
+}
+
+const fns = [
+  { fn: promise(r => fnWithCb(null, "arg", r)), expect: "arg" },
+  {
+    fn: promise(r => fnWithCb(new Error("arg"), null, r)),
+    expect: e => e instanceof Error
+  }
+]
+
+module.exports = fns
