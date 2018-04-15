5e30743791642b8419a07d566793c9c3849450de
Jascha Ehrenreich <jascha@jaeh.at>
2018-04-15 20:27:27 +0200

    export promise from lib

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const run = require("./run")
+const { promise } = require("./lib")
 
 process.env.NODE_ENV = process.env.NODE_ENV || "test"
 
 run.run = run
+
+run.promise = promise
 
 module.exports = run
