02467441b0becd7edac1b819b252ee3338f3e1b5
Jascha Ehrenreich <jascha@jaeh.at>
2018-04-16 04:24:12 +0200

    add -p args to test

--- src/bin/t.js
+++ src/bin/t.js
@@ -1,5 +1,15 @@
 #!/usr/bin/env node
 
+const a = process.argv
+
+const ps = ['-p', '--prod', '--production']
+
+const prod = ps.some(p => a.indexOf(p) > -1)
+
+if (prod) {
+  process.env.NODE_ENV = 'production'
+}
+
 if (process.env.NODE_ENV === 'production') {
   require('./unit')
 } else {
