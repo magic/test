fde357c5eaa425f30cdb81e69840920d1e53b7db
Jascha Ehrenreich <jascha@jaeh.at>
2018-04-17 22:39:17 +0200

    add storage tests

--- test/index.js
+++ test/index.js
@@ -2,6 +2,8 @@
 const vals = require('./vals')
 // library util functions
 const lib = require('./lib')
+// mem store for test stats
+const storage = require('./storage')
 // complicated test structure
 const structure = require('./structure')
 // export spec
@@ -11,7 +13,8 @@ const tests = () => ({
   vals,
   lib,
   spec,
-  // structure,
+  storage,
+  structure,
 })
 
 module.exports = tests
---
+++ test/storage.js
@@ -0,0 +1,28 @@
+const is = require('@magic/types')
+
+const store = require('../src/storage')
+
+const before = () => () => store.set('key', undefined)
+
+const fns = [
+  { fn: () => store === store.store, expect: true },
+  { fn: () => store.set, expect: is.fn },
+  { fn: () => store.get, expect: is.fn },
+  { fn: () => store.suites, expect: is.obj },
+  { fn: () => store.module, expect: '@magic/test' },
+  { fn: () => store.stats, expect: is.obj },
+  { fn: () => store.stats.all, expect: is.num },
+  { fn: () => store.stats.pass, expect: is.num },
+  { fn: () => store.stats.fail, expect: is.num },
+  {
+    fn: () => store.set('key', 'value'),
+    expect: () => store.get('key') === 'value',
+    before,
+  },
+  {
+    fn: () => store.get('key'),
+    expect: undefined,
+  },
+]
+
+module.exports = fns
