03c8a18fee1ac857c1d19f7ad21a9735496c0d72
Jascha Ehrenreich <jascha@jaeh.at>
2018-04-17 20:57:01 +0200

    export log and types from test

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 const run = require('./run')
 const { promise } = require('./lib')
+const log = require('@magic/log')
+const is = require('@magic/types')
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'test'
 
 run.run = run
 
 run.promise = promise
+
+run.log = log
+
+run.is = is
 
 module.exports = run
