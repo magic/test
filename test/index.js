33706c58608c02ed7a88afc6d40c485caf89a9ea
Jascha Ehrenreich <jascha@jaeh.at>
2018-04-21 03:52:03 +0200

    simplify export structure

--- test/index.js
+++ test/index.js
@@ -1,23 +1,14 @@
-// standard vals that get exported
-const vals = require('./vals')
-// library util functions
-const lib = require('./lib')
-// mem store for test stats
-const storage = require('./storage')
-// complicated test structure
-const structure = require('./structure')
-// export spec
-const spec = require('./spec')
-// format/prettier config
-const format = require('./format')
-
-const tests = () => ({
-  vals,
-  lib,
-  spec,
-  storage,
-  structure,
-  format,
-})
-
-module.exports = tests
+module.exports = {
+  // standard vals that get exported
+  vals: require('./vals'),
+  // library util functions
+  lib: require('./lib'),
+  // mem store for test stats
+  storage: require('./storage'),
+  // complicated test structure
+  structure: require('./structure'),
+  // export spec
+  spec: require('./spec'),
+  // format/prettier config
+  format: require('./format'),
+}
