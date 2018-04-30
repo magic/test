62ca93b28668dbea066c78dbfd1e2922dadab24b
Jascha Ehrenreich <jascha@jaeh.at>
2018-04-30 22:26:59 +0200

    fix recursion

--- src/mock/log.js
+++ src/mock/log.js
@@ -1,7 +1,8 @@
+const { isProd } = require('../../src')
 const log = require('@magic/log')
 
 module.exports = {
-  log: (...args) => log.info(args),
+  log: (...args) => isProd && console.log(args) || false,
   warn: (...args) => log.info(args),
   error: (...args) => log.info(args),
 }
