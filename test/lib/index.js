49b6cef758105afc3d422acf8b04b3ada5f573ac
Jascha Ehrenreich <jascha@jaeh.at>
2018-04-30 17:43:30 +0200

    remove index files, not needed

--- test/lib/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  cleanError: require('./cleanError'),
-  cleanFunctionString: require('./cleanFunctionString'),
-  curry: require('./curry'),
-  expectedArguments: require('./expectedArguments'),
-  promise: require('./promise'),
-  deepMerge: require('./deepMerge'),
-}
