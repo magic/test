e3e974a72a680979c5fe1a889be3427daa1fe1c4
Jascha Ehrenreich <jascha@jaeh.at>
2018-04-30 22:58:14 +0200

    errors always log

--- src/mock/log.js
+++ src/mock/log.js
@@ -4,5 +4,5 @@ const log = require('@magic/log')
 module.exports = {
   log: (...args) => isProd && console.log(args) || false,
   warn: (...args) => log.info(args),
-  error: (...args) => log.info(args),
+  error: (...args) => log.error(args),
 }
--- test/mock/log.js
+++ test/mock/log.js
@@ -4,5 +4,5 @@ module.exports = [
   { fn: mock.log, expect: is.object },
   { fn: mock.log.log('test'), expect: isProd ? false : undefined },
   { fn: mock.log.warn('test'), expect: isProd ? false : undefined },
-  { fn: mock.log.error('test'), expect: isProd ? false : undefined },
+  { fn: mock.log.error('test'), expect: undefined },
 ]
