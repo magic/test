46857144872cc62d46258ffa270a5b413561cb91
Jascha Ehrenreich <jascha@jaeh.at>
2018-04-30 16:44:15 +0200

    unit can now independently find tests

--- src/bin/unit.js
+++ src/bin/unit.js
@@ -1,12 +1,40 @@
 #!/usr/bin/env node
 const path = require('path')
+const fs = require('fs')
 
 const log = require('@magic/log')
 
 const run = require('../run')
 
-const testDir = path.join(process.cwd(), 'test')
-const tests = require(testDir)
+const readRecursive = dir => {
+  const testDir = path.join(process.cwd(), 'test')
+  const targetDir = !dir ? testDir : path.join(testDir, dir)
+
+  let tests = {}
+
+  fs.readdirSync(targetDir).forEach(file => {
+    if (file.indexOf('.') === 0) {
+      return
+    }
+
+    const filePath = path.join(targetDir, file)
+    const stat = fs.statSync(filePath)
+
+    if (stat.isDirectory()) {
+      tests = {
+        ...tests,
+        ...readRecursive(dir ? path.join(dir, file) : file),
+      }
+    } else if (stat.isFile()) {
+      const fileP = filePath.replace(testDir, '')
+      tests[fileP] = require(filePath)
+    }
+  })
+
+  return tests
+}
+
+const tests = readRecursive()
 
 if (!tests) {
   log.error('NO tests specified')
