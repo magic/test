8809230b0644e6a767338caba753d7e5175d71a2
Jascha Ehrenreich <jascha@jaeh.at>
2018-04-30 17:44:34 +0200

    test argument types for test objects

--- test/structure.js
+++ test/structure.js
@@ -1,5 +1,7 @@
 const is = require('@magic/types')
 
+const { promise } = require('../src')
+
 process.env.testVar = ''
 // test before function
 const before = t => {
@@ -10,6 +12,8 @@ const before = t => {
     delete process.env.testVar
   }
 }
+
+const cbFn = (e, a, cb) => cb(e, a)
 
 module.exports = {
   // test possible test structure
@@ -37,6 +41,42 @@ module.exports = {
         { fn: () => 'string', expect: 'string' },
       ],
     },
+  },
+  argumentTypes: {
+    primitive: [
+      { fn: true, info: 'can pass literal value into test fn' },
+      { fn: false, expect: false, info: 'can pass literal value into test fn' },
+      { fn: 'test', expect: 'test', info: 'can pass literal value into test fn' },
+    ],
+    function: [
+      { fn: () => true, info: 'can pass function into fn' },
+      { fn: () => false, expect: false, info: 'can pass function into fn' },
+      { fn: () => 't', expect: 't', info: 'can pass function into fn' },
+      { fn: true, expect: () => true, info: 'can pass function into expect' },
+      { fn: false, expect: () => false, info: 'can pass function into expect' },
+      { fn: 't', expect: () => 't', info: 'can pass function into expect' },
+      { fn: () => true, expect: () => true, info: 'can pass function into fn and expect' },
+      { fn: () => false, expect: () => false, info: 'can pass function into fn and expect' },
+      { fn: () => 't', expect: () => 't', info: 'can pass function into fn and expect' },
+      { fn: 'test', expect: 'test', info: 'can pass literal value into test fn' },
+    ],
+    promise: [
+      { fn: new Promise(r => r(true)), info: 'pass Promise into test fn' },
+      { fn: new Promise(r => r()), expect: undefined, info: 'pass Promise into test fn' },
+      { fn: new Promise(r => r('t')), expect: 't', info: 'pass Promise into test fn' },
+      { fn: true, expect: new Promise(r => r(true)), info: 'pass Promise into expect' },
+      { fn: false, expect: new Promise(r => r(false)), info: 'pass Promise into expect' },
+      { fn: 't', expect: new Promise(r => r('t')), info: 'pass Promise into expect' },
+    ],
+    callback: [
+      { fn: promise(r => cbFn(null, 't', r)), expect: 't', info: 'can handle cbs in fn' },
+      { fn: true, expect: promise(r => cbFn(null, true, r)), info: 'can handle cbs in expect' },
+      {
+        fn: promise(r => cbFn(null, true, r)),
+        expect: promise(r => cbFn(null, true, r)),
+        info: 'can handle cbs in fn and expect',
+      },
+    ],
   },
   testNestedArray: {
     arr: [
