95581387e27e07cc20c0b37847b56676e49bea27
Jascha Ehrenreich <jascha@jaeh.at>
2018-05-12 03:29:43 +0200

    unneeded log

--- src/mock/log.js
+++ src/mock/log.js
@@ -3,7 +3,6 @@ const log = require('@magic/log')
 const { isNodeProd } = require('../env')
 
 const cons = console
-cons.error({ isNodeProd  })
 
 module.exports = {
   log: (...args) => isNodeProd() && cons.log(...args) || false,
