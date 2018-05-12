338d010dc6b16dd03491ec8dd03ef017da40963b
Jascha Ehrenreich <jascha@jaeh.at>
2018-05-12 03:23:38 +0200

    tests pass independent of env

--- test/mock/log.js
+++ test/mock/log.js
@@ -2,7 +2,7 @@ const { is, mock, isNodeProd } = require('../../src')
 
 module.exports = [
   { fn: mock.log, expect: is.object },
-  { fn: mock.log.log('test'), expect: isNodeProd() ? false : true },
-  { fn: mock.log.warn('test'), expect: isNodeProd() ? false : true },
+  { fn: mock.log.log('test'), expect: t => isNodeProd() ? t === false : t === true },
+  { fn: mock.log.warn('test'), expect: t => isNodeProd() ? t === false : t === true },
   { fn: mock.log.error('test'), expect: undefined },
 ]
