8de85a74ae864e41a0cd929fc2de8fa435e19bea
Jascha <jascha@jaeh.at>
2018-11-09 17:29:24 +0100

    no more recursion, yay

--- src/lib/mock/log.js
+++ src/lib/mock/log.js
@@ -5,7 +5,7 @@ const env = require('../env')
 const cons = console
 
 module.exports = {
-  log: (...args) => (env.isNodeProd() && console.log(...args)) || false,
+  log: (...args) => (env.isNodeProd() && cons.log(...args)) || false,
   warn: (...args) => log.info(args),
-  error: (...args) => console.error(...args),
+  error: (...args) => cons.error(...args),
 }
