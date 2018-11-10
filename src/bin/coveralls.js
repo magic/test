6cbba7d2f1da0034609dac3c4016fe36773469c3
Jascha <jascha@jaeh.at>
2018-11-10 19:49:19 +0100

    coveralls should resolve nyc correctly now

--- src/bin/coveralls.js
+++ src/bin/coveralls.js
@@ -2,10 +2,28 @@
 const { exec } = require('child_process')
 const path = require('path')
 
+const { name } = require(path.join(process.cwd(), 'package.json'))
+
 const cwd = process.cwd()
+
 const nodeModules = path.join(cwd, 'node_modules')
 
-const nycCliPath = path.join(nodeModules, 'nyc', 'bin', 'nyc.js')
+let nycCliPath
+if (name === '@magic/test') {
+  nycCliPath = path.join(nodeModules, '.bin', 'nyc')
+} else {
+  nycCliPath = path.join(
+    nodeModules,
+    '@magic',
+    'test',
+    'node_modules',
+    '.bin',
+    'nyc',
+  )
+}
+
 const coverallsPath = path.join(nodeModules, 'coveralls', 'bin', 'coveralls.js')
 
-exec(`${nycCliPath} report --reporter=text-lcov | ${coverallsPath}`, (...a) => console.log(...a))
+const cmd = `${nycCliPath} report --reporter=text-lcov | ${coverallsPath}`
+const cb = console.log
+exec(cmd, cb)
