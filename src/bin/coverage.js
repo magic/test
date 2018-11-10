57c94b1614c47631a61185979764b0a9fb71191c
Jascha <jascha@jaeh.at>
2018-11-10 23:08:59 +0100

    fix nyc paths

--- src/bin/coverage.js
+++ src/bin/coverage.js
@@ -6,13 +6,11 @@ const nodeModules = path.join(cwd, 'node_modules')
 const { name } = require(path.join(cwd, 'package.json'))
 
 // find clipaths
-let cliPath
+let cliPath = path.join(nodeModules, '.bin', 'nyc')
 let cmd
 if (name === '@magic/test') {
   cmd = path.join(cwd, 'src', 'bin', 'unit.js')
-  cliPath = path.join(nodeModules, '.bin', 'nyc')
 } else {
-  cliPath = path.join(cwd, 'node_modules', '@magic', 'test', 'node_modules', '.bin', 'nyc')
   cmd = path.join(cwd, 'node_modules', '@magic', 'test', 'src', 'bin', 'unit.js')
 }
 
