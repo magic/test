85163b812a39b073469dacb818a48800c3e6901b
Jascha <jascha@jaeh.at>
2018-12-08 17:25:50 +0100

    add cleanError test

--- test/lib/cleanError.js
+++ test/lib/cleanError.js
@@ -5,11 +5,13 @@ const { cleanError } = require('../../src/lib')
 const error = new Error('testerror')
 
 module.exports = [
-  { fn: () => cleanError(error), expect: is.array },
-  { fn: () => cleanError(error), expect: is.len.eq(2) },
+  { fn: cleanError(error), expect: is.array },
+  { fn: cleanError(error), expect: is.len.eq(2) },
   {
-    fn: () => cleanError(error),
-    expect: ([e]) => e.indexOf('testerror') !== -1,
+    fn: cleanError(error),
+    expect: ([e]) => e.includes('testerror'),
   },
-  { fn: () => cleanError(error), expect: ([_, e]) => e.indexOf('at') === 0 },
+  { fn: cleanError(error), expect: ([_, e]) => e.startsWith('at') },
+  { fn: cleanError(error), expect: ([_, e]) => e.startsWith('at') },
+  { fn: cleanError('test'), expect: 'test', info: 'errors without file property get returned unchanged' },
 ]
