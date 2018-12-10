7a16707183084225dfefd0b3f4ed756a9ee366b4
Jascha <jascha@jaeh.at>
2018-12-10 12:51:24 +0100

    log.error returns true

--- src/lib/mock/log.js
+++ src/lib/mock/log.js
@@ -7,5 +7,5 @@ const cons = console
 module.exports = {
   log: (...args) => (env.isNodeProd() && cons.log(...args)) || false,
   warn: (...args) => log.info(args),
-  error: (...args) => cons.error(...args),
+  error: (...args) => cons.error(...args) || true,
 }
--- test/lib/mock/log.js
+++ test/lib/mock/log.js
@@ -1,8 +1,10 @@
 const { is, mock, env } = require('../../../src')
 
+const testLogging = t => (env.isNodeProd() ? t === false : t === true)
+
 module.exports = [
   { fn: mock.log, expect: is.object },
-  { fn: mock.log.log('test'), expect: t => (env.isNodeProd() ? t === false : t === true) },
-  { fn: mock.log.warn('test'), expect: t => (env.isNodeProd() ? t === false : t === true) },
-  { fn: mock.log.error('test'), expect: undefined },
+  { fn: mock.log.log('test'), expect: testLogging, info: 'log.log logs in prod but not dev' },
+  { fn: mock.log.warn('test'), expect: testLogging, info: 'log.warn logs in prod but not dev'},
+  { fn: mock.log.error('test'), expect: true, info: 'log.error always returns undefined' },
 ]
