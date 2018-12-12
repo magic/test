9c50ea103ae40049f07d5b810739cf130ae459b8
Jascha <jascha@jaeh.at>
2018-12-12 06:54:58 +0100

    fix test info

--- test/lib/mock/log.js
+++ test/lib/mock/log.js
@@ -6,5 +6,5 @@ module.exports = [
   { fn: mock.log, expect: is.object },
   { fn: mock.log.log('test'), expect: testLogging, info: 'log.log logs in prod but not dev' },
   { fn: mock.log.warn('test'), expect: testLogging, info: 'log.warn logs in prod but not dev' },
-  { fn: mock.log.error('test'), expect: true, info: 'log.error always returns undefined' },
+  { fn: mock.log.error('test'), expect: true, info: 'log.error always returns true' },
 ]
