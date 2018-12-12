e8a04b4b5abd5e17787f7547247c528027e5a80d
Jascha <jascha@jaeh.at>
2018-12-12 07:10:59 +0100

    add time and timeEnd to log mock

--- src/lib/mock/log.js
+++ src/lib/mock/log.js
@@ -1,11 +1,9 @@
-const log = require('@magic/log')
-
 const env = require('../env')
 
-const cons = console
-
 module.exports = {
-  log: (...args) => (env.isNodeProd() && cons.log(...args)) || false,
-  warn: (...args) => log.info(args),
-  error: (...args) => cons.error(...args) || true,
+  log: () => !env.isNodeProd(),
+  warn: () =>!env.isNodeProd(),
+  error: () => true,
+  time: () => !env.isNodeProd(),
+  timeEnd: () => !env.isNodeProd(),
 }
--- test/lib/mock/log.js
+++ test/lib/mock/log.js
@@ -1,10 +1,12 @@
 const { is, mock, env } = require('../../../src')
 
-const testLogging = t => (env.isNodeProd() ? t === false : t === true)
+const mayLog = t => (env.isNodeProd() ? t === false : t === true)
 
 module.exports = [
   { fn: mock.log, expect: is.object },
-  { fn: mock.log.log('test'), expect: testLogging, info: 'log.log logs in dev but not prod' },
-  { fn: mock.log.warn('test'), expect: testLogging, info: 'log.warn logs in dev but not prod' },
+  { fn: mock.log.log('test'), expect: mayLog, info: 'log.log logs in dev but not prod' },
+  { fn: mock.log.warn('test'), expect: mayLog, info: 'log.warn logs in dev but not prod' },
   { fn: mock.log.error('test'), expect: true, info: 'log.error always returns true' },
+  { fn: mock.log.time('test'), expect: mayLog, info: 'log.time logs in dev but not prod' },
+  { fn: mock.log.timeEnd('test'), expect: mayLog, info: 'log.timeEnd logs in dev but not prod' },
 ]
