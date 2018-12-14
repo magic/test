d54507c9dfa531049ea360e529e987c704ff866a
Jascha <jascha@jaeh.at>
2018-12-14 18:38:25 +0100

    format

--- src/lib/mock/log.js
+++ src/lib/mock/log.js
@@ -2,7 +2,7 @@ const env = require('../env')
 
 module.exports = {
   log: () => !env.isNodeProd(),
-  warn: () =>!env.isNodeProd(),
+  warn: () => !env.isNodeProd(),
   error: () => true,
   time: () => !env.isNodeProd(),
   timeEnd: () => !env.isNodeProd(),
