368bae2d1f8a68afc057f3fcfd2eb854a396452b
Jascha <jascha@jaeh.at>
2019-05-13 02:32:22 +0200

    add argv for includes

--- src/bin/coverage.js
+++ src/bin/coverage.js
@@ -6,39 +6,25 @@ const nodeModules = path.join(cwd, 'node_modules')
 const { name } = require(path.join(cwd, 'package.json'))
 
 // find clipaths
-let cliPath = path.join(nodeModules, '.bin', 'nyc')
+let cliPath = path.join(nodeModules, '.bin', 'c8')
 let cmd = path.join(cwd, 'node_modules', '@magic', 'test', 'src', 'bin', 'unit.js')
 if (name === '@magic/test') {
   cmd = path.join(cwd, 'src', 'bin', 'unit.js')
 }
 
 const init = argv => {
-  let args = ['--colors']
+  let args = []
 
-  if (argv['--exclude']) {
-    const excludes = Array.from(
-      new Set([
-        'coverage/**',
-        'packages/*/test/**',
-        'test/**',
-        'test{,-*}.js',
-        '**/*{.,-}test.js',
-        '**/__tests__/**',
-        '**/{ava,babel,jest,nyc,rollup,webpack}.config.js',
-        ...argv['--exclude'],
-      ]),
-    )
-    excludes.forEach(ex => {
-      args.push('--exclude')
-      args.push(`"${ex}"`)
-    })
+  args.push('-n')
+  args.push('src')
+  if (argv['--includes']) {
+    args = [...args, argv['--includes']]
   }
-
-  args.push('-a')
 
   args.push('node')
   args.push(cmd)
 
   cli.spawn(cliPath, args)
 }
+
 module.exports = init
