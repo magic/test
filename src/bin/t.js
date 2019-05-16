b0dfde7b52577bbc47311c60451e5a9808dca237
Jascha <jascha@jaeh.at>
2019-05-16 03:07:00 +0200

    c8 gets loaded on appveyor?

--- src/bin/t.js
+++ src/bin/t.js
@@ -9,9 +9,11 @@ const { name } = require(path.join(cwd, 'package.json'))
 let binPath = path.join(cwd, 'node_modules', '@magic', 'test', 'src', 'bin')
 if (name === '@magic/test') {
   binPath = path.join(cwd, 'src', 'bin')
+
 }
 
 const cmd = path.join(binPath, 'unit.mjs')
 const argv = ['-n', 'src', 'node', '--experimental-json-modules', '--experimental-modules', cmd]
 
-spawn(argv, path.join(cwd, 'node_modules', '.bin', 'c8'))
+let c8Path = path.join(cwd, 'node_modules', 'c8', 'bin', 'c8.js')
+spawn(argv, c8Path)
