8c54ed754f796e7a93c499384cddeec351d117fb
jascha <jascha@jaeh.at>
2020-02-13 23:38:57 +0100

    use cli, execute coveralls directly instead of spawning it

--- src/bin/calls.mjs
+++ src/bin/calls.mjs
@@ -3,6 +3,9 @@
 import child_process from 'child_process'
 import path from 'path'
 
+import coveralls from 'coveralls'
+
+import cli from '@magic/cli'
 import log from '@magic/log'
 
 const cwd = process.cwd()
@@ -12,17 +15,18 @@ const nodeModules = path.join(cwd, 'node_modules')
 const c8CliPath = path.join(nodeModules, '.bin', 'c8')
 const coverallsPath = path.join(nodeModules, '.bin', 'coveralls')
 
-const cmd = `${c8CliPath} report --reporter=text-lcov | ${coverallsPath}`
+const cmd = `${c8CliPath} report --reporter=text-lcov`
 
-child_process.exec(cmd, (err, stdout, stderr) => {
-  if (err) {
-    log.error(err)
+const run = async () => {
+  try {
+    const coverage = await cli.exec(cmd)
+
+    const calls = await new Promise(res => coveralls.handleInput(coverage, res))
+    log(calls)
+  } catch (e) {
+    log.error(e)
     process.exit(1)
   }
-  if (stderr) {
-    log.error(stderr)
-    process.exit(1)
-  }
+}
 
-  log(stdout)
-})
+run()
