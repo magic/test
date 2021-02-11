83b695ac2b7e21c0adcd2c0b099e38714b6506a2
jaeh <jascha@jaeh.at>
2021-02-11 01:21:31 +0100

    test/html.mjs: add test if props get passed

--- test/html.mjs
+++ test/html.mjs
@@ -1,8 +1,16 @@
+const html = true
+
 export default [
   {
     fn: () => i(['testing', p('testing')]),
     expect: ['i', ['testing', ['p', 'testing']]],
     info: 'html i is defined',
-    html: true,
+    html,
+  },
+  {
+    fn: () => i({ class: 'testing' }, 'testing'),
+    expect: ['i', { class: 'testing' }, 'testing'],
+    info: '@magic/test can now test html',
+    html,
   },
 ]
