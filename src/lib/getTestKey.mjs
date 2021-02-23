9f49224e8ccff2b086d3df3604b3c54196f422a1
jaeh <jascha@jaeh.at>
2021-02-23 21:02:40 +0100

    add lib/getTestKey

---
+++ src/lib/getTestKey.mjs
@@ -0,0 +1,23 @@
+export const getTestKey = (pkg, parent, name) => {
+  let key = ''
+  if (parent && parent !== pkg) {
+    key = `${pkg}`
+  }
+  if (parent && parent !== name) {
+    if (!parent.startsWith('/') && parent !== pkg) {
+      parent = `.${parent}`
+    }
+    key += `${parent}`
+  }
+
+  if (name) {
+    if (parent && parent !== name && parent !== pkg && !name.startsWith('/')) {
+      key += '#'
+    } else if (!name.startsWith('/')) {
+      name = `/${name}`
+    }
+
+    key += name
+  }
+  return key
+}
