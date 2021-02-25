4008c61e5c1fd7a1c41fd9ed02aa7e3ab507a9ef
jaeh <jascha@jaeh.at>
2021-02-25 05:00:54 +0100

    index: change export structure

--- src/index.mjs
+++ src/index.mjs
@@ -1,20 +1,20 @@
-export { default as log } from '@magic/log'
+import logging from '@magic/log'
+
 export { default as is } from '@magic/types'
 export { default as deep } from '@magic/deep'
 export { default as fs } from '@magic/fs'
 
-import runIt from './run.mjs'
+export { run } from './run.mjs'
 
 export { curry, env, mock, promise, store, vals, version, tryCatch } from './lib/index.mjs'
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'test'
 
+export const log = logging
+
 export const isProd = process.env.NODE_ENV === 'production'
 export const isTest = process.env.NODE_ENV === 'test'
 export const isDev = process.env.NODE_ENV === 'development'
-
-export const run = runIt
-export default runIt
 
 process
   .on('unhandledRejection', error => {
