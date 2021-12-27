c2ce7b4a86c2db238224008343b338ab00a53ccc
jascha <j@jaeh.at>
2021-12-27 02:06:50 +0100

    bin/calls: remove unused variable

--- src/bin/calls.mjs
+++ src/bin/calls.mjs
@@ -12,7 +12,6 @@ const cwd = process.cwd()
 const nodeModules = path.join(cwd, 'node_modules')
 
 const c8CliPath = path.join(nodeModules, '.bin', 'c8')
-const coverallsPath = path.join(nodeModules, '.bin', 'coveralls')
 
 const cmd = `${c8CliPath} report --reporter=text-lcov`
 
