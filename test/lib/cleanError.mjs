a1d84b7c4b3343eeabca09bffe128207a8dfa257
jaeh <jascha@jaeh.at>
2023-03-14 20:03:40 +0100

    add some lib tests

---
+++ test/lib/cleanError.mjs
@@ -0,0 +1,14 @@
+import { is } from '../../src/index.mjs'
+import { cleanError } from '../../src/lib/index.mjs'
+
+export default [
+  { fn: cleanError, expect: undefined, info: 'empty argument returns argument' },
+  { fn: cleanError(false), expect: false, info: '"false" as argument returns argument' },
+  { fn: cleanError(true), expect: true, info: '"true" as argument returns argument' },
+  { fn: cleanError(true), expect: true, info: '"true" as argument returns argument' },
+  { fn: cleanError(new Error('testing')), expect: t => t[0] === 'Error: testing', info: 'cleaned Error gets "Error: ${message}" expanded as expected' },
+  { fn: cleanError(new Error('testing')), expect: t => t[1].includes('cleanError.'), info: 'cleaned Error gets first line of stack returned as cleanedError[1] expanded as expected' },
+  { fn: cleanError({ stack: 'testing', }), expect: t => is.undefined(t[1]), info: 'cleaned Error without multiline stack gets returned with only one stack item' },
+  { fn: cleanError({ stack: 'testing', }), expect: t => t[0] === 'testing', info: 'cleaned Error without multiline stack gets returned with correct first stack item' },
+
+]
---
+++ test/lib/stringify.mjs
@@ -0,0 +1,12 @@
+import { is } from '../../src/index.mjs'
+import { stringify } from '../../src/lib/stringify.mjs'
+
+export default [
+  { fn: stringify, expect: undefined, info: 'empty argument returns argument' },
+  { fn: stringify(false), expect: false, info: '"false" as argument returns "false"' },
+  { fn: stringify(true), expect: true, info: '"true" as argument returns "true"' },
+  { fn: stringify({}), expect: {}, info: 'empty object returns empty object' },
+  { fn: stringify([]), expect: [], info: 'empty array returns empty array' },
+  { fn: stringify(['testing', () => {}]), expect: ['testing', '() => {}'], info: 'empty array returns empty array' },
+  { fn: stringify({ testing: true, fn: () => {} }), expect: { testing: true, fn: '() => {}' }, info: 'functions get stringifiede' },
+]
