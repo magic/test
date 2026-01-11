eb01826e9aac0fa06fbe5b2059f387884b99eeb8
Jascha Ehrenreich <jascha@artificialmuseum.com>
2026-01-11 16:07:06 +0100

    tryCatch: remove unneeded if that checks if args are empty, instead pass the empty args

--- src/lib/tryCatch.js
+++ src/lib/tryCatch.js
@@ -8,10 +8,6 @@ export const tryCatch =
   (fn, ...args) =>
   async () => {
     try {
-      if (args.length === 0) {
-        return await fn()
-      }
-
       return await fn(...args)
     } catch (e) {
       return e
