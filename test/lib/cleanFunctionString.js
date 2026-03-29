e32b3a1202e588f83965d19c3b8ffdf6c542e55c
Jascha Ehrenreich <jascha@artificialmuseum.com>
2026-03-29 05:57:45 +0200

    test/lib/cleanFunctionString: remove ts-ignore

--- test/lib/cleanFunctionString.js
+++ test/lib/cleanFunctionString.js
@@ -2,39 +2,26 @@ import { cleanFunctionString } from '../../src/lib/index.js'
 
 export default [
   { fn: () => cleanFunctionString(async () => true), expect: 'true' },
-  /** @ts-ignore */
   { fn: () => cleanFunctionString(async t => true), expect: 'true' },
-  /** @ts-ignore */
   { fn: () => cleanFunctionString(async t => true), expect: 'true' },
-
-  /** @ts-ignore */
   { fn: () => cleanFunctionString(t => true), expect: 'true' },
-  /** @ts-ignore */
   { fn: () => cleanFunctionString(t => true), expect: 'true' },
   { fn: () => cleanFunctionString(() => true), expect: 'true' },
-
   {
     fn: () => cleanFunctionString((async () => true).toString()),
     expect: 'true',
   },
   {
-    /** @ts-ignore */
     fn: () => cleanFunctionString((async t => true).toString()),
     expect: 'true',
   },
   {
-    /** @ts-ignore */
     fn: () => cleanFunctionString((async t => true).toString()),
     expect: 'true',
   },
-
-  /** @ts-ignore */
-  { fn: () => cleanFunctionString((t => true).toString()), expect: 'true' },
-  /** @ts-ignore */
-  { fn: () => cleanFunctionString((t => true).toString()), expect: 'true' },
-  { fn: () => cleanFunctionString((() => true).toString()), expect: 'true' },
-
-  /** @ts-ignore */
-  { fn: () => cleanFunctionString(), expect: 'false' },
-  { fn: () => cleanFunctionString(1), expect: '1' },
+  { fn: cleanFunctionString((t => true).toString()), expect: 'true' },
+  { fn: cleanFunctionString((t => true).toString()), expect: 'true' },
+  { fn: cleanFunctionString((() => true).toString()), expect: 'true' },
+  { fn: cleanFunctionString(), expect: 'false' },
+  { fn: cleanFunctionString(1), expect: '1' },
 ]
