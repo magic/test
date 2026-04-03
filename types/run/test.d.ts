962bbcbddce554a7c081d68beceda830e0685a80
Jascha Ehrenreich <jascha@artificialmuseum.com>
2026-04-03 18:18:10 +0200

    update types

--- types/run/test.d.ts
+++ types/run/test.d.ts
@@ -1,3 +1,3 @@
-export function runTest(test: Test, store?: Store): Promise<TestResult | Suite | undefined>
+export function runTest(test: WrappedTest, store?: Store): Promise<TestResult | Suite | undefined>
 import { Store } from '../lib/store.js'
 //# sourceMappingURL=test.d.ts.map
