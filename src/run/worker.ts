9cbf816444699a425132aa2b50cc2727c5cbdeff
Jascha Ehrenreich <jascha@artificialmuseum.com>
2026-04-17 23:16:12 +0200

    Revert "format"
    This reverts commit 9087886b6e053c604f60180ec7bcbf21519a02b6.

--- src/run/worker.ts
+++ src/run/worker.ts
@@ -5,13 +5,7 @@ import is from '@magic/types'
 import { cleanError, cleanFunctionString, getTestKey } from '../lib/index.js'
 import { restoreFromSnapshot } from './isolation.js'
 import { getViteDefine } from '../lib/svelte/viteConfig/index.js'
-import type {
-  WrappedTest,
-  CleanupFunction,
-  TestResult,
-  EvaluateResult,
-  Snapshot,
-} from '../types.js'
+import type { WrappedTest, CleanupFunction, TestResult, EvaluateResult, Snapshot } from '../types.js'
 
 import '../bin/lib/registerLoader.js'
 
