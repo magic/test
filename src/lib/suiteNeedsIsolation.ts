353450dedec9d7bcd2e2952bcd035334537d1790
Jascha Ehrenreich <jascha@artificialmuseum.com>
2026-05-06 13:20:00 +0200

    lib/suiteNeedsIsolation: use TestCollection as type, was Test[] | TestObject

--- src/lib/suiteNeedsIsolation.ts
+++ src/lib/suiteNeedsIsolation.ts
@@ -1,5 +1,5 @@
 import is from '@magic/types'
-import type { Test, TestCollection, TestObject } from '../types.js'
+import type { TestCollection } from '../types.js'
 
 /**
  * Check if any test in the suite needs isolation (has before/after hooks)
@@ -8,21 +8,19 @@ export const suiteNeedsIsolation = (tests: TestCollection): boolean => {
   if (is.array(tests)) {
     return tests.some(test => is.function(test.before) || is.function(test.after))
   } else if (is.objectNative(tests)) {
-    const testObj = tests as TestObject
-    return Object.values(testObj).some(test => {
-      if (
-        is.objectNative(test) &&
-        (is.function((test as Test).before) || is.function((test as Test).after))
-      ) {
-        return true
-      }
-
-      if (is.objectNative(test) && (test as Test).tests) {
-        return suiteNeedsIsolation((test as Test).tests as TestCollection)
-      }
-
+    if (!is.objectNative(tests)) {
       return false
-    })
+    }
+
+    if (is.function(tests.before) || is.function(tests.after)) {
+      return true
+    }
+    if (tests.tests && is.objectNative(tests.tests)) {
+      return suiteNeedsIsolation(tests.tests)
+    }
+    if (tests.nested && is.objectNative(tests.nested)) {
+      return suiteNeedsIsolation(tests.nested)
+    }
   }
 
   return false
