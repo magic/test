ec30317f8cf8c4427cef02975d2b348a01da8306
Jascha Ehrenreich <jascha@artificialmuseum.com>
2026-05-06 14:39:51 +0200

    test/lib/suiteNeedsIsolation: add ts-expect-error to tests with invalid arguments

--- test/lib/suiteNeedsIsolation.ts
+++ test/lib/suiteNeedsIsolation.ts
@@ -13,16 +13,19 @@ export default [
     info: 'detects after in nested tests object',
   },
   {
+    // @ts-expect-error invalid argument
     fn: () => suiteNeedsIsolation('invalid'),
     expect: false,
     info: 'returns false for non-object/array input',
   },
   {
+    // @ts-expect-error invalid argument
     fn: () => suiteNeedsIsolation(123),
     expect: false,
     info: 'returns false for number input',
   },
   {
+    // @ts-expect-error invalid argument
     fn: () => suiteNeedsIsolation(null),
     expect: false,
     info: 'returns false for null input',
