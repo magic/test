8a2fb4897caf18acbe451af4ab29279ea8294e7a
Jascha Ehrenreich <jascha@artificialmuseum.com>
2026-06-27 17:53:30 +0200

    refactor: simplify suiteHasBeforeAllOrAfterAll condition

--- dist/run/lib/suiteHasBeforeAllOrAfterAll.js
+++ dist/run/lib/suiteHasBeforeAllOrAfterAll.js
@@ -1,9 +1,7 @@
 import is from '@magic/types'
 export const suiteHasBeforeAllOrAfterAll = tests => {
   if (is.object(tests) && !is.arr(tests)) {
-    const hasBefore = 'beforeAll' in tests && is.fn(tests.beforeAll)
-    const hasAfter = 'afterAll' in tests && is.fn(tests.afterAll)
-    return hasBefore || hasAfter
+    return is.fn(tests.beforeAll) || is.fn(tests.afterAll)
   }
   return false
 }
