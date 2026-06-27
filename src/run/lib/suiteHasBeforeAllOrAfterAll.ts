cbab969d4c8bfab3b83d7638e0d23387c9bf8ef2
Jascha Ehrenreich <jascha@artificialmuseum.com>
2026-06-27 15:01:36 +0200

    refactor(suiteHasBeforeAllOrAfterAll): simplify boolean check

--- src/run/lib/suiteHasBeforeAllOrAfterAll.ts
+++ src/run/lib/suiteHasBeforeAllOrAfterAll.ts
@@ -3,9 +3,7 @@ import type { TestCollection } from '../../types.ts'
 
 export const suiteHasBeforeAllOrAfterAll = (tests: TestCollection): boolean => {
   if (is.object(tests) && !is.arr(tests)) {
-    const hasBefore = 'beforeAll' in tests && is.fn(tests.beforeAll)
-    const hasAfter = 'afterAll' in tests && is.fn(tests.afterAll)
-    return hasBefore || hasAfter
+    return is.fn(tests.beforeAll) || is.fn(tests.afterAll)
   }
   return false
 }
